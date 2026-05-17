e8aec977e4a95c2c75f956013207290de5465a0a
Nicholas Yang <58143649+NicholasY2509@users.noreply.github.com>
2026-05-17 16:37:28 +0700

    fix

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,3 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Schedule::command('app:open-daily-cash-log')->dailyAt('07.30')->timezone('Asia/Jakarta');
-Schedule::command('app:close-daily-cash-log')->dailyAt('21.00')->timezone('Asia/Jakarta');
-Schedule::command('app:close-stall-assignments')->dailyAt('21.00')->timezone('Asia/Jakarta');
